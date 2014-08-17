4dffda353201f588bffbe512bb30f3c9879c28c0
Otter <otc@otterthecat.com>
2014-08-17 18:04:56 -0500

    now using a Collection View

--- public/javascripts/view.js
+++ public/javascripts/view.js
@@ -1,10 +1,14 @@
+// view for individual robot models
 var RobotView = Backbone.View.extend({
 
-	el: $('#robots'),
+	tagName: 'li',
 
 	render: function(model){
 		this.model = model;
 		this.$el.html("Here is a robot name: " + this.model.get('name'));
+
+		// return view for chaining
+		return this;
 	},
 
 	events: {
@@ -14,6 +18,32 @@ var RobotView = Backbone.View.extend({
 	}
 });
 
-var robotView = new RobotView();
+// view to run all views for collection
+// acts as a sort of 'mini controller' for
+// the above RobotView
+var RobotCollectionView = Backbone.View.extend({
 
-robotView.render(r1);
+	// target element in which to update view
+	el: $('#robots'),
+
+	// tag name to be auto inserted to the above el
+	// if we didn't define it, would default to a div
+	tagName: 'ul',
+
+	render: function(collection){
+
+		collection.each(function(robot){
+
+			var roboTemplate = new RobotView();
+			this.$el.append(roboTemplate.render(robot).el);
+		}.bind(this));
+
+		// return view for chaining
+		return this;
+	}
+});
+
+var robotColletionView = new RobotCollectionView();
+
+// 'myRobots collection from courtesy of collection.js
+robotColletionView.render(myRobots);
