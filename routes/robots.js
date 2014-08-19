d9523ff7003b1dc7a145a96a03a5bb768a9f3c4e
Otter <otc@otterthecat.com>
2014-08-18 20:11:33 -0500

    updated express route to send fake data on get request

--- routes/robots.js
+++ routes/robots.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+router.param('id', function(req, res, next, id){
+	next();
+});
+
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get('/:id', function(req, res) {
+  res.json({
+    	id: 1,
+    	name: 'hal 9000',
+    	firepower: 'sentient ray'
+    });
 });
 
 router.post('/', function(req, res){
